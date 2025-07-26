84cb98bb9c882b9b466aa4a3e4f54f3ae07c988d
Lucas Silva <lucasapsilva123@gmail.com>
2025-07-26 18:59:48 -0300

    Corrigindo a rota para banckend

--- inject.js
+++ inject.js
@@ -37,7 +37,7 @@ search.addEventListener('click', async () => {
             list = null
         }
 
-        const response = await fetch(`https://consultasnbackend-production.up.railway.app`, {
+        const response = await fetch(`https://consultasnbackend-production.up.railway.app/search-cnpj`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
